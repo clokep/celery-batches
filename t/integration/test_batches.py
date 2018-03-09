b0f0221723dca4cab104bad7d13ecf24f3ae57c5
Patrick Cloke <clokep@patrick.cloke.us>
2018-03-09 07:24:40 -0500

    Check additional task signals.

--- t/integration/test_batches.py
+++ t/integration/test_batches.py
@@ -3,9 +3,21 @@ from __future__ import absolute_import, unicode_literals
 
 from time import sleep
 
-from celery.signals import task_prerun, task_postrun
+from celery import signals
 
 from .tasks import add, cumadd, Results
+
+
+class SignalCounter(object):
+    def __init__(self, expected_calls):
+        self.calls = 0
+        self.expected_calls = expected_calls
+
+    def __call__(self, *args, **kwargs):
+        self.calls += 1
+
+    def assert_calls(self):
+        assert self.calls == self.expected_calls
 
 
 def test_flush_interval(celery_worker):
@@ -44,17 +56,27 @@ def test_result(celery_worker):
 
 def test_signals(celery_app, celery_worker):
     """The batch runs after two calls."""
-    class SignalCounter(object):
-        def __init__(self):
-            self.calls = 0
-
-        def __call__(self, task_id, task, args, kwargs, **kw):
-            self.calls += 1
-
-    prerun = SignalCounter()
-    task_prerun.connect(prerun)
-    postrun = SignalCounter()
-    task_postrun.connect(postrun)
+    # Configure a SignalCounter for each task signal.
+    checks = (
+        # Each task request gets published separately.
+        (signals.before_task_publish, 2),
+        (signals.after_task_publish, 2),
+        # The task only runs a single time.
+        (signals.task_prerun, 1),
+        (signals.task_postrun, 1),
+        # Other task signals are not implemented.
+        (signals.task_retry, 0),
+        (signals.task_success, 0),
+        (signals.task_failure, 0),
+        (signals.task_revoked, 0),
+        (signals.task_unknown, 0),
+        (signals.task_rejected, 0),
+    )
+    signal_counters = []
+    for sig, expected_count in checks:
+        counter = SignalCounter(expected_count)
+        sig.connect(counter)
+        signal_counters.append(counter)
 
     add.delay(1)
     add.delay(3)
@@ -66,9 +88,8 @@ def test_signals(celery_app, celery_worker):
     # Should still have the correct result.
     assert Results().get() == 4
 
-    # The task should have only run once.
-    assert prerun.calls == 1
-    assert postrun.calls == 1
+    for counter in signal_counters:
+        counter.assert_calls()
 
 
 # TODO
