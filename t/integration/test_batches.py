1944f7ecc7516b3bccec0a884e7efdbd5ab9f63f
Patrick Cloke <clokep@patrick.cloke.us>
2018-03-08 22:40:25 -0500

    Add basic tests for signals.

--- t/integration/test_batches.py
+++ t/integration/test_batches.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, unicode_literals
 
 from time import sleep
 
+from celery.signals import task_prerun, task_postrun
+
 from .tasks import add, cumadd, Results
 
 
@@ -40,6 +42,34 @@ def test_result(celery_worker):
     assert result_2.get(timeout=3) == 3
 
 
+def test_signals(celery_app, celery_worker):
+    """The batch runs after two calls."""
+    class SignalCounter(object):
+        def __init__(self):
+            self.calls = 0
+
+        def __call__(self, task_id, task, args, kwargs, **kw):
+            self.calls += 1
+
+    prerun = SignalCounter()
+    task_prerun.connect(prerun)
+    postrun = SignalCounter()
+    task_postrun.connect(postrun)
+
+    add.delay(1)
+    add.delay(3)
+
+    # Yield control to the other thread. (This needs to be shorter than the
+    # flush interval.)
+    sleep(1)
+
+    # Should still have the correct result.
+    assert Results().get() == 4
+
+    # The task should have only run once.
+    assert prerun.calls == 1
+    assert postrun.calls == 1
+
+
 # TODO
-# * Test signals
 # * Test acking
